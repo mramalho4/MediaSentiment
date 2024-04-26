5681d9bb1c5b3fb5566ebde56f80e9f156f03353
mramalho4 <111776332+mramalho4@users.noreply.github.com>
2024-04-26 11:34:39 -0400

    Delete find_keywords.R

--- find_keywords.R
+++
@@ -1,7 +0,0 @@
-# Function to find keywords
-find_keywords <- function(text, threshold = 1) {
-  word_freq <- calculate_word_frequency(text)
-  keywords <- names(word_freq[word_freq >= threshold])
-  return(keywords)
-}
-
